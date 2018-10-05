a4ac479e18dc3a0234ea657429a2cfb3d32fcc53
Virginia Dooley <vcdooley@gmail.com>
2018-10-05 11:25:36 +0000

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,17 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+require_relative "../lib/keys.rb"
+
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
 end
+
+puts get_the_keys(groceries)
